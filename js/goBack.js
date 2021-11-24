f4a155b0462b3b131e889266412e2ca6c9fbd78c
Marlene Carli <maarlen.car@hotmail.com>
2021-11-24 13:02:56 -0300

    Agregado intervalo para redirección en template de error

--- js/goBack.js
+++ js/goBack.js
@@ -6,3 +6,6 @@ goBack_BTN.addEventListener('click', goBack);
 function goBack() {  //Da funcionalidad al botón de volver atrás
     window.history.back();
 }
+setInterval(() => {
+    goBack();
+}, 2000);
